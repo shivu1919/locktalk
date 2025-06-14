eaff5369be38e4a93a7196a332adbaa1e750d463
shivu1919 <shivamthakur.ss129@gmail.com>
2025-06-14 20:55:58 +0530

    final commit

--- src/Secret.jsx
+++ src/Secret.jsx
@@ -11,6 +11,11 @@ function Secret() {
   const[password, setPassword] = useState('')
   const[hint,setHint] =useState('')
   const navigate = useNavigate()
+  const[eform, SetEForm] = useState(true)
+  const[dform, SetDForm] = useState(false)
+  const[decryptedMsg, setDecryptedMsg] = useState('')
+  const[copiedSecretMsg, setCopiedSecretMsg] = useState('')
+  const[passwordForDecryption, setPasswordForDecryption] = useState('')
 
 
   const encryptMessage = ()=>{
@@ -23,16 +28,37 @@ function Secret() {
             state:{
                 hint_msg : hint, 
                 secret_msg : secretmsg}})
-      })
-
-      
+      })  
   }
+
+    const decryptMessage = ()=>{
+      axios.post(`http://localhost:8080/decryptMsg?secret=${encodeURIComponent(copiedSecretMsg)}&password=${encodeURIComponent(passwordForDecryption)}`)
+      .then((response) => {
+        const secretmsg = response.data;
+        setDecryptedMsg(secretmsg);
+        console.log(secretmsg)
+      })  
+  }
+
+
 
   return (
     <>
       <div className='container'>
             <h1>Lock - Talk</h1>
-            <div id="form">
+            <div style={{display:"flex", columnGap:"20px"}}>
+              <button className={eform? 'active' : 'disable'} onClick={()=>{
+                SetEForm(true)
+                SetDForm(false)
+                }}>Encrypt</button>
+
+              <button className={dform? 'active':'disable'} onClick={()=>{
+                SetDForm(true)
+                SetEForm(false)
+              }}>Decrypt</button>
+            </div>
+
+            {eform && <div id="form">
               <label>Message:</label>
 
               <textarea
@@ -60,7 +86,37 @@ function Secret() {
               onChange={(event)=>{setHint(event.target.value)}}
               />
               <button onClick={encryptMessage}>Encrypt Message</button>
-            </div>
+            </div>}
+
+            {dform &&             
+            <div id="decrypt-form">
+              <label>Secret Message:</label>
+              <input
+              type='text'
+              placeholder='Copy & paste the secret message'
+              value={copiedSecretMsg}
+              onChange={(event)=>{
+                setCopiedSecretMsg(event.target.value)
+              }}
+              />
+
+              <label>Password:</label>
+              <input
+              placeholder="Decode the password from it's Hint"
+              value={passwordForDecryption}
+              onChange={(event)=>setPasswordForDecryption(event.target.value)}
+              />
+
+              <button onClick={decryptMessage}>Decrypt Message</button>
+
+
+              <input
+              readOnly
+              value={decryptedMsg}
+              />
+            </div>}
+
+
       </div>
     </>
   )
--- src/Success.jsx
+++ src/Success.jsx
@@ -21,14 +21,16 @@ function Success() {
                 justifyContent:"center"
             }}
             >
-
-                <h1>Your message has been secured</h1>
-                <h2>You can copy & send the blow given content to your friend</h2>
+                <div style={{border:"1px solid black", display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"centet",rowGap:"10px",padding:"12px",borderRadius:"12px"}}>
+                         <h1>Your message has been secured</h1>
+                <h1>You can copy & send the blow given content to your friend</h1>
                 <h2>Copy the secret message in the decoder box and type the password based on the Hint that I have provided</h2>
-                <h3>Secret message is : {secret_msg}</h3>
-                <h3>password Hint is: {hint_msg}</h3>
+                <h3>Secret message : {secret_msg}</h3>
+                <h3>Password hint : {hint_msg}</h3>
 
-                <Link to="/">Go to Home</Link>
+                <Link to="/"><button>Go to Home </button></Link>
+                </div>
+               
             </div>
    </>
   )
